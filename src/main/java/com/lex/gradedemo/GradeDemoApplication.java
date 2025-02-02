da5d998c65e8fdd28e513c2320d7b41bb6d5ea4c
xle <xu.le@autogeneral.com.au>
2025-02-02 23:37:23 +1000

    commit test

--- src/main/java/com/lex/gradedemo/GradeDemoApplication.java
+++ src/main/java/com/lex/gradedemo/GradeDemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GradeDemoApplication {
 
+
+    //test。  xxx
     public static void main(String[] args) {
         SpringApplication.run(GradeDemoApplication.class, args);
     }
