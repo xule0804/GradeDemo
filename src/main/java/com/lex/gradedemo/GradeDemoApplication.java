d9b5e5a1d79b4094c0728aca96ff4a12aab32b03
xle <xu.le@autogeneral.com.au>
2025-02-02 18:01:46 +1000

    Initial commit

---
+++ src/main/java/com/lex/gradedemo/GradeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lex.gradedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradeDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lex/gradedemo/GradeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lex.gradedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
